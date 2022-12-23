9f246b3f6523154002345ac7aa5c095c3985ac33
MrUmbele <braisico@gmail.com>
2022-12-23 08:58:18 +0100

    Create Project

---
+++ src/main/java/com/formacion/spring/TeamRocketBackApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRocketBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamRocketBackApplication.class, args);
+	}
+
+}
